58d40e9d0bad798c7988d0ee1892863936353153
Solange Domingues <solange.domingues.oficial@gmail.com>
2020-11-26 14:55:54 -0300

    Project Structure.

---
+++ src/main/java/br/com/solangedomingues/transferapi/TransferApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.solangedomingues.transferapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransferApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransferApiApplication.class, args);
+	}
+
+}
